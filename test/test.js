b4f5ba89faf3ee357e840f9b58d1b2907afaf639
吉岡大希 <hey.daikichi339@gmail.com>
2016-01-21 23:41:49 +0900

    testコードを追加

--- test/test.js
+++ test/test.js
@@ -2,6 +2,10 @@ casper.test.begin('name', function suite(test){
   //user agent
   casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36');
 
+  casper.on('page.error', function (msg, trace) {
+    this.echo( 'Error: ' + msg, 'ERROR' );
+  });
+
   casper.start('../index.html', function(){
     this.sendKeys('#test_input', 'testです');
     this.click('#test_submit');
@@ -9,7 +13,6 @@ casper.test.begin('name', function suite(test){
   });
 
   casper.then(function(){
-    this.capture('../test/screen/test_02.png');
     test.assertTitle('title1 | description');
   });
   
